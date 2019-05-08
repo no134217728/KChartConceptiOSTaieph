3e333b125ee91fd7df93f4c58d590334c401bbef
Wei Jen Wang <weijen.wang@infinox.cn>
2019-05-08 10:44:35 +0800

    feat(Main): add required double extension for this demo

--- KChartDemoiOSTaipeh/YourMarketsListsViewController.swift
+++ KChartDemoiOSTaipeh/YourMarketsListsViewController.swift
@@ -41,3 +41,11 @@ enum ContentOffsetType {
 class YourMarketsListsViewController: UIViewController {
     
 }
+
+extension Double {
+    /// Rounds the double to decimal places value
+    func roundTo(places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
