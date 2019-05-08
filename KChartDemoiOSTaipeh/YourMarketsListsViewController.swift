4d04cea94df1447214105ecad1d654477a58f376
Wei Jen Wang <weijen.wang@infinox.cn>
2019-05-08 10:43:58 +0800

    feat(Main): add required enums for the demo

--- KChartDemoiOSTaipeh/YourMarketsListsViewController.swift
+++ KChartDemoiOSTaipeh/YourMarketsListsViewController.swift
@@ -8,6 +8,36 @@
 
 import UIKit
 
+enum PositionSystem {
+    case Right
+    case Left
+    case TechRight
+    case TechLeft
+}
+
+enum KTechType {
+    case None
+    case MA
+    case BOLL
+}
+
+enum TechType {
+    case None
+    case ARBR
+    case ATR
+    case BIAS
+    case CCI
+    case MACD
+    case KD
+    case KDJ
+    case RSI
+}
+
+enum ContentOffsetType {
+    case Current
+    case ToLast
+}
+
 class YourMarketsListsViewController: UIViewController {
     
 }
