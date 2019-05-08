7ce188365a05bf43b37f4122fc2e2b36c8976c85
Wei Jen Wang <weijen.wang@infinox.cn>
2019-05-08 10:17:22 +0800

    feat(Main): connect items on ChartUsingPage

--- KChartDemoiOSTaipeh/YourChartUsingViewController.swift
+++ KChartDemoiOSTaipeh/YourChartUsingViewController.swift
@@ -9,5 +9,40 @@
 import UIKit
 
 class YourChartUsingViewController: UIViewController {
+    @IBOutlet weak var todaysButton: UIButton!
+    @IBOutlet weak var candlesButton: UIButton!
+    @IBOutlet weak var maButton: UIButton!
+    @IBOutlet weak var bollButton: UIButton!
+    @IBOutlet weak var arbrButton: UIButton!
+    @IBOutlet weak var macdButton: UIButton!
+
+    @IBAction func todaysClicked() {
+        
+    }
     
+    @IBAction func candlesClicked() {
+        
+    }
+
+    @IBAction func maClicked() {
+        
+    }
+    
+    @IBAction func bollClicked() {
+        
+    }
+    
+    @IBAction func arbrClicked() {
+        
+    }
+    
+    @IBAction func macdClicked() {
+        
+    }
+    
+    var chartViewController: ChartViewController?
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        chartViewController = segue.destination as? ChartViewController
+    }
 }
