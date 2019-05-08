9cc5b881ac44bb9b5b288c491b3a82ea87fc6358
Wei Jen Wang <weijen.wang@infinox.cn>
2019-05-08 10:36:56 +0800

    feat(Main): connect items on ChartViewController

--- KChartDemoiOSTaipeh/ChartViewController.swift
+++ KChartDemoiOSTaipeh/ChartViewController.swift
@@ -9,5 +9,40 @@
 import UIKit
 
 class ChartViewController: UIViewController {
+    @IBOutlet weak var kLineView: UIView!
+    @IBOutlet weak var topLabelsView: UIView!
+    @IBOutlet weak var leftLabelsView: UIView!
+    @IBOutlet weak var gridView: UIView!
+    @IBOutlet weak var chartsScrollView: UIScrollView!
+    @IBOutlet weak var chartUsingRightView: UIView!
+    @IBOutlet weak var rightLabelsView: UIView!
+    @IBOutlet weak var bottomLabelsView: UIView!
     
+    @IBOutlet weak var techView: UIView!
+    @IBOutlet weak var techTopLablesView: UIView!
+    @IBOutlet weak var techLeftLabelsView: UIView!
+    @IBOutlet weak var techGridView: UIView!
+    @IBOutlet weak var techScrollView: UIScrollView!
+    @IBOutlet weak var techRightLabelsView: UIView!
+    @IBOutlet weak var techBottomLabelsView: UIView!
+    
+    @IBOutlet weak var currentLineView: UIView!
+    @IBOutlet weak var currentPriceLabel: UILabel!
+    
+    @IBOutlet weak var constraintTopLabelsHeight: NSLayoutConstraint!
+    @IBOutlet weak var constraintLeftLabelsWidth: NSLayoutConstraint!
+    @IBOutlet weak var constraintChartUsingRightWidth: NSLayoutConstraint!
+    @IBOutlet weak var constraintRightLabelsWidth: NSLayoutConstraint!
+    @IBOutlet weak var constraintBottomLabelsHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var constraintTechViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var constraintTechTopHeight: NSLayoutConstraint!
+    @IBOutlet weak var constraintTechUsingRightWidth: NSLayoutConstraint!
+    @IBOutlet weak var constraintTechBottomHeight: NSLayoutConstraint!
+}
+
+extension ChartViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+    }
 }
