530f5193a591a07991bf58bf1e37afd171c87f00
Wei Jen Wang <weijen.wang@infinox.cn>
2019-05-08 09:28:05 +0800

    Initial Commit

---
+++ KChartDemoiOSTaipeh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KChartDemoiOSTaipeh
+//
+//  Created by Wei Jen Wang on 2019/5/8.
+//  Copyright © 2019 Wei Jen Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
